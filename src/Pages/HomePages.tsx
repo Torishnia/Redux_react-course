7e44bbf09ea341dc9cf75ce906a9ec23261627e7
Torishnia <valeri.torishnya@gmail.com>
2023-02-03 16:15:03 +0300

    Connect github Api to my project

--- src/Pages/HomePages.tsx
+++ src/Pages/HomePages.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useSearchUsersQuery } from '../store/github/github.api';
 
 export default function HomePages() {
+  const { isLoading, isError, data } = useSearchUsersQuery('valeri');
+
+  console.log(data);
+  
+
   return (
     <div>home</div>
   )
